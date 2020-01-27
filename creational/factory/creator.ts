6467824c293e1b0fcec26f3b1a63fe7422b402b6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-27 13:53:01 +0100

    The creator abstract class

---
+++ creational/factory/creator.ts
@@ -0,0 +1,10 @@
+import { Product } from './product';
+
+export default abstract class Creator {
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): String {
+        const product = this.factoryMethod();
+        return `Creator: The same creator's code has just worked with ${product.operation()}`;
+    }
+}
