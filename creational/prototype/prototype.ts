4e0ff6270bf197f368cf39f52ea575a3bdfebec6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-29 17:49:54 +0100

    Prototype with clone method

---
+++ creational/prototype/prototype.ts
@@ -0,0 +1,16 @@
+export class Prototype {
+    public primitive: any;
+    public component: object;
+    public circularReference: ComponentWithBackReference;
+
+    public clone(): this {
+        const clone = Object.create(this);
+        clone.component = Object.create(this.component);
+        clone.circularReference = {
+            ...this.circularReference,
+            prototype: { ...this },
+        };
+
+        return clone;
+    }
+}
