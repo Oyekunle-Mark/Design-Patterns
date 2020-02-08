60d8e0a6608fb66f4a63e46b90974381273e9189
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-08 22:47:58 +0100

    Instantiated the client

--- structural/adapter/client.ts
+++ structural/adapter/client.ts
@@ -2,7 +2,22 @@ import { Adaptee } from './adaptee';
 import { Adapter } from './adapter';
 import { Target } from './target';
 
-function clientCode(target: Target) {
+function clientCode(target: Target): void {
     target.request();
 }
 
+console.log('Client: I can work just fine with the Target objects:');
+const target = new Target();
+clientCode(target);
+
+console.log('');
+
+const adaptee = new Adaptee();
+console.log('Client: The Adaptee class has a weird interface. See, I don\'t understand it:');
+console.log(`Adaptee: ${adaptee.specificRequest()}`);
+
+console.log('');
+
+console.log('Client: But I can work with it via the Adapter:');
+const adapter = new Adapter(adaptee);
+clientCode(adapter);
