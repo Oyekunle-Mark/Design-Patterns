7f5b738770ebcdb85a231473d3c23fd9d594e9c8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-09 14:56:25 +0100

    Extended abstraction

---
+++ structural/extendedAbstraction.ts
@@ -0,0 +1,8 @@
+import { Abstraction } from './abstraction';
+
+export class ExtendedAbstraction extends Abstraction {
+    public operation(): string {
+        const result = this.implementation.operationImplementation();
+        return `ExtendedAbstraction: Extended operation with:\n${result}`;
+    }
+}
