be93211098016e9208557eb322ac2652b4b6993e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 20:02:55 +0100

    Builds a simple and complex tree

--- structural/composite/client.ts
+++ structural/composite/client.ts
@@ -5,3 +5,23 @@ import { Leaf } from './leaf';
 function clientCode(component: Component): void {
     console.log(`RESULT: ${component.operation()}`);
 }
+
+const simple = new Leaf();
+console.log('Client: I\'ve got a simple component:');
+clientCode(simple);
+
+console.log('');
+
+const tree = new Composite();
+const branch1 = new Composite();
+branch1.add(new Leaf());
+branch1.add(new Leaf());
+const branch2 = new Composite();
+branch2.add(new Leaf());
+tree.add(branch1);
+tree.add(branch2);
+
+console.log('Client: Now I\'ve got a composite tree:');
+clientCode(tree);
+
+console.log('');
