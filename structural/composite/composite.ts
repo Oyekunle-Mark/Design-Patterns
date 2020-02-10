6bb475df40d48451caa29d18b10dd5a05f62e733
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 19:51:57 +0100

    composite type

---
+++ structural/composite/composite.ts
@@ -0,0 +1,31 @@
+import { Component } from './component';
+
+export class Composite extends Component {
+    protected children: Component[] = [];
+
+    public add(component: Component): void {
+        this.children.push(component);
+        component.setParent(this);
+    }
+
+    public remove(component: Component): void {
+        const componentIndex = this.children.indexOf(component);
+        this.children.splice(componentIndex, 1);
+
+        component.setParent(null);
+    }
+
+    public isComposite(): boolean {
+        return true;
+    }
+
+    public operation(): string {
+        const results = [];
+
+        for (const child of this.children) {
+            results.push(child.operation());
+        }
+
+        return `Branch(${results.join('+')})`;
+    }
+}
