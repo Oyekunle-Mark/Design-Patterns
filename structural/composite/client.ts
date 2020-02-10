7f254e7a36f4a2ed2b8d5be3c8332f3f41571b54
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 20:03:52 +0100

    Client code with complex signature

--- structural/composite/client.ts
+++ structural/composite/client.ts
@@ -25,3 +25,14 @@ console.log('Client: Now I\'ve got a composite tree:');
 clientCode(tree);
 
 console.log('');
+
+function clientCode2(component1: Component, component2: Component): void {
+    if (component1.isComposite()) {
+        component1.add(component2);
+    }
+
+    console.log(`RESULT: ${component1.operation()}`);
+}
+
+console.log('Client: I don\'t need to check the components classes even when managing the tree:');
+clientCode2(tree, simple);
