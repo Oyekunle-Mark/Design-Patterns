a14519e78c6fd0848a770d60b44e4d0e38b1c61b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 19:40:47 +0100

    The component

---
+++ structural/composite/component.ts
@@ -0,0 +1,21 @@
+export abstract class Component {
+    protected parent: Component;
+
+    public setParent(component: Component): void {
+        this.parent = component;
+    }
+
+    public getParent(): Component {
+        return this.parent;
+    }
+
+    public add(component: Component): void { }
+
+    public remove(component: Component): void { }
+
+    public isComposite(): boolean {
+        return false;
+    }
+
+    public abstract operation(): string;
+}
