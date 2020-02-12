75b2aff80ff65bf54fb9084fd74089c50473d09e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-12 16:25:16 +0100

    Client code

--- structural/decorator/client.ts
+++ structural/decorator/client.ts
@@ -1,3 +1,7 @@
 import { Component } from './component';
 import { ConcreteComponent } from './concreteComponent';
 import { ConcreteDecoratorA, ConcreteDecoratorB } from './concreteDecorator';
+
+function clientCode(component: Component): void {
+    console.log(`RESULT: ${component.operation()}`);
+}
