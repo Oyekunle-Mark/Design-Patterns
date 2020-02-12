f5abced0ff81e27f5a1925c022948d14bb362992
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-12 16:27:20 +0100

    Using a decorated component

--- structural/decorator/client.ts
+++ structural/decorator/client.ts
@@ -11,3 +11,8 @@ console.log('Client: I\'ve got a simple component:');
 clientCode(simple);
 
 console.log('');
+
+const decorator1 = new ConcreteDecoratorA(simple);
+const decorator2 = new ConcreteDecoratorB(decorator1);
+console.log('Client: Now I\'ve got a decorated component:');
+clientCode(decorator2);
