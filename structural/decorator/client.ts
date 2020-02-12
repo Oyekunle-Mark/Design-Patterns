c2488817ccd3b9b6a2b62ff1e7a70809b2d25ee3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-12 16:25:57 +0100

    Using a simple component

--- structural/decorator/client.ts
+++ structural/decorator/client.ts
@@ -5,3 +5,9 @@ import { ConcreteDecoratorA, ConcreteDecoratorB } from './concreteDecorator';
 function clientCode(component: Component): void {
     console.log(`RESULT: ${component.operation()}`);
 }
+
+const simple = new ConcreteComponent();
+console.log('Client: I\'ve got a simple component:');
+clientCode(simple);
+
+console.log('');
