8837bb58889b8d3de209f6c094aca6894341eb2a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 10:57:12 +0100

    Facade sets up and uses the subsystems

--- structural/facade/facade.ts
+++ structural/facade/facade.ts
@@ -5,4 +5,15 @@ export class Facade {
         private subSystem1: SubSystem1 = new SubSystem1(),
         private subSystem2: SubSystem2 = new SubSystem2(),
     ) { }
+
+    public operation(): string {
+        let result = 'Facade initializes subsystems:\n';
+        result += this.subSystem1.operation1();
+        result += this.subSystem2.operation1();
+        result += 'Facade orders subsystems to perform the action:\n';
+        result += this.subSystem1.operationN();
+        result += this.subSystem2.operationZ();
+
+        return result;
+    }
 }
