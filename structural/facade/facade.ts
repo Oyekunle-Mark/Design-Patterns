6ebc6fcd563f065fd12a65756d6e80292e428c15
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 10:56:05 +0100

    Refactored the facade class

--- structural/facade/facade.ts
+++ structural/facade/facade.ts
@@ -1,5 +1,8 @@
 import { SubSystem1, SubSystem2 } from './subSystems';
 
 export class Facade {
-    
+    constructor(
+        private subSystem1: SubSystem1 = new SubSystem1(),
+        private subSystem2: SubSystem2 = new SubSystem2(),
+    ) { }
 }
