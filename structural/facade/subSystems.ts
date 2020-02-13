a896e6cd4c78e93a94ad2da77e159e3b2a7d39d5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 10:50:24 +0100

    The second sub system

--- structural/facade/subSystems.ts
+++ structural/facade/subSystems.ts
@@ -1,9 +1,19 @@
 export class SubSystem1 {
-    public operation(): string {
+    public operation1(): string {
         return 'Subsystem1: Ready!\n';
     }
 
     public operationN(): string {
         return 'Subsystem1: Go!\n';
+    }
+}
+
+export class SubSystem2 {
+    public operation1(): string {
+        return 'Subsystem2: Get ready!\n';
+    }
+
+    public operationZ(): string {
+        return 'Subsystem2: Fire!';
     }
 }
