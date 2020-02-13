4cd18c1a9a7fa5ec09fbc9c8519b4c350f8abd0d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 10:49:44 +0100

    The first sub system

---
+++ structural/facade/subSystems.ts
@@ -0,0 +1,9 @@
+export class SubSystem1 {
+    public operation(): string {
+        return 'Subsystem1: Ready!\n';
+    }
+
+    public operationN(): string {
+        return 'Subsystem1: Go!\n';
+    }
+}
