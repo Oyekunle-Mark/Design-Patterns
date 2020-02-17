b157237e31e85c041e1a486a949c09e7a1b25f39
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-17 10:31:06 +0100

    pre-populated flyweights in the  * initialization stage of the application.

---
+++ structural/flyweight/client.ts
@@ -0,0 +1,12 @@
+import { FlyweightFactory } from './flyweightFactory';
+
+const factory = new FlyweightFactory([
+    ['Chevrolet', 'Camaro2018', 'pink'],
+    ['Mercedes Benz', 'C300', 'black'],
+    ['Mercedes Benz', 'C500', 'red'],
+    ['BMW', 'M5', 'red'],
+    ['BMW', 'X6', 'white'],
+    // ...
+]);
+
+factory.listFlyweights();
