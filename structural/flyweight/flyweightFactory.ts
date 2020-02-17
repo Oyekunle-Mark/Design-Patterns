e226a0e541b88ad0758d25124c65ad1acdb395cf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-17 10:16:38 +0100

    Gets a flyweight

--- structural/flyweight/flyweightFactory.ts
+++ structural/flyweight/flyweightFactory.ts
@@ -13,4 +13,17 @@ export class FlyweightFactory {
     private getKey(state: string[]): string {
         return state.join('_');
     }
+
+    public getFlyweight(sharedState: string[]): Flyweight {
+        const key = this.getKey(sharedState);
+
+        if (!(key in this.flyweights)) {
+            console.log('FlyweightFactory: Can\'t find a flyweight, creating new one.');
+            this.flyweights[key] = new Flyweight(sharedState);
+        } else {
+            console.log('FlyweightFactory: Reusing existing flyweight.');
+        }
+
+        return this.flyweights[key];
+    }
 }
