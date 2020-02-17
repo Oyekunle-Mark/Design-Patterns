c82e7a45b6e1847adb25ac6002be9d8a11beb83b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-17 13:20:02 +0100

    Can check acess and log request before delegating to the service

--- structural/proxy/proxy.ts
+++ structural/proxy/proxy.ts
@@ -1,13 +1,23 @@
 import { Subject } from './subject';
 import { ConcreteSubject } from './concreteSubject';
 
-class Proxy implements Subject {
-    constructor(private subject: ConcreteSubject) {}
+export class Proxy implements Subject {
+    constructor(private concreteSubject: ConcreteSubject) {}
 
     public request(): void {
         if (this.checkAccess()) {
-            this.realSubject.request();
+            this.concreteSubject.request();
             this.logAccess();
         }
+    }
+
+    private checkAccess(): boolean {
+        console.log('Proxy: Checking access prior to firing a real request.');
+
+        return true;
+    }
+
+    private logAccess(): void {
+        console.log('Proxy: Logging the time of request.');
     }
 }
