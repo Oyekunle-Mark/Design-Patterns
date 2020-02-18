fcbd09d27610c570cca606e913d00fd5263948d8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 13:31:30 +0100

    Hanldes a request

--- behavioural/cof/abstractHandler.ts
+++ behavioural/cof/abstractHandler.ts
@@ -8,4 +8,12 @@ export class AbstractHandler implements Handler {
 
         return handler;
     }
+
+    public handle(request: string): string | null {
+        if (this.nextHandler) {
+            return this.nextHandler.handle(request);
+        }
+
+        return null;
+    }
 }
