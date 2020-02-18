989d16cfaa216de6784848db69fab6d9293e67da
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 13:40:07 +0100

    dog handler

--- behavioural/cof/concreteHanlders.ts
+++ behavioural/cof/concreteHanlders.ts
@@ -18,3 +18,12 @@ export class SquirrelHandler extends AbstractHandler {
         return super.handle(request);
     }
 }
+
+export class DogHandler extends AbstractHandler {
+    public handle(request: string): string | null {
+        if (request === 'MeatBall') {
+            return `Dog: I'll eat the ${request}.`;
+        }
+        return super.handle(request);
+    }
+}
