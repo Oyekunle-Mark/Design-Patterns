cceb2e7481b6c25cfd35f81da633658fe4deb77b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 13:49:15 +0100

    Simulates the client code

--- behavioural/cof/client.ts
+++ behavioural/cof/client.ts
@@ -16,3 +16,16 @@ function clientCode(handler: Handler): void {
         }
     }
 }
+
+const monkey = new MonkeyHandler();
+const squirrel = new SquirrelHandler();
+const dog = new DogHandler();
+
+monkey.setNext(squirrel).setNext(dog);
+
+console.log('Chain: Monkey > Squirrel > Dog\n');
+clientCode(monkey);
+console.log('');
+
+console.log('Subchain: Squirrel > Dog\n');
+clientCode(squirrel);
