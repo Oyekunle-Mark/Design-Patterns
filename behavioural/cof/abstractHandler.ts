e673122f7b9801c5c0e56c27a8b69395052e41c0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 13:28:55 +0100

    Sets the next handler

--- behavioural/cof/abstractHandler.ts
+++ behavioural/cof/abstractHandler.ts
@@ -2,4 +2,10 @@ import { Handler } from './handler';
 
 export class AbstractHandler implements Handler {
     private nextHandler: Handler;
+
+    public setNext(handler: Handler): Handler {
+        this.nextHandler = handler;
+
+        return handler;
+    }
 }
