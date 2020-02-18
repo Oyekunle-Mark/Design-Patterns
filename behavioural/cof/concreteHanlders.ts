ddf32d788fe6845025d99e1a188a754ace87b0dc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-18 13:38:45 +0100

    Squirrel handler

--- behavioural/cof/concreteHanlders.ts
+++ behavioural/cof/concreteHanlders.ts
@@ -9,3 +9,12 @@ export class MonkeyHandler extends AbstractHandler {
         return super.handle(request);
     }
 }
+
+export class SquirrelHandler extends AbstractHandler {
+    public handle(request: string): string | null {
+        if (request === 'Nut') {
+            return `Squirrel: I'll eat the ${request}.`;
+        }
+        return super.handle(request);
+    }
+}
