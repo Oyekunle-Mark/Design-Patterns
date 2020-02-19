81909828bc2d47ab5ac982da6f08d33f72ec4a61
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-19 10:43:29 +0100

    Delegates in the execute method

--- behavioural/command/complexCommand.ts
+++ behavioural/command/complexCommand.ts
@@ -3,4 +3,10 @@ import { Receiver } from './receiver';
 
 export class ComplexCommand implements Command {
     constructor(private receiver: Receiver, private a: string, private b: string) { }
+
+    public execute(): void {
+        console.log('ComplexCommand: Complex stuff should be done by a receiver object.');
+        this.receiver.doSomething(this.a);
+        this.receiver.doSomethingElse(this.b);
+    }
 }
