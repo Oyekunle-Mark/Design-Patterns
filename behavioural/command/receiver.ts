05a34e808199af30992258fb9ad93d0f079dc710
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-19 10:39:17 +0100

    The receiver receives and handles requests

---
+++ behavioural/command/receiver.ts
@@ -0,0 +1,9 @@
+export class Receiver {
+    public doSomething(a: string): void {
+        console.log(`Receiver: Working on (${a}.)`);
+    }
+
+    public doSomethingElse(b: string): void {
+        console.log(`Receiver: Also working on (${b}.)`);
+    }
+}
