545bff256a502830c2cce16cca95a09fc3f6f633
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-20 18:43:18 +0100

    Concrete iterator implements iterator interface

--- behavioural/iterator/concreteIterator.ts
+++ behavioural/iterator/concreteIterator.ts
@@ -9,4 +9,31 @@ export class AlphabeticalOrderIterator implements Iterator<string> {
             this.position = collection.getCount() - 1;
         }
     }
+
+    public rewind(): void {
+        this.position = this.reverse ? this.collection.getCount() - 1 : 0;
+    }
+
+    public current(): any {
+        return this.collection.getItems()[this.position];
+    }
+
+    public key(): number {
+        return this.position;
+    }
+
+    public next(): any {
+        const item = this.collection.getItems()[this.position];
+        this.position += this.reverse ? -1 : 1;
+
+        return item;
+    }
+
+    public valid(): boolean {
+        if (this.reverse) {
+            return this.position > 0;
+        }
+
+        return this.position < this.collection.getCount();
+    }
 }
