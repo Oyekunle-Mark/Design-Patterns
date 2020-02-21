dd7287c3779048d3b3d5ac4d6f13984383dc3d98
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 12:52:59 +0100

    Changed the access modifier

--- behavioural/mediator/baseComponent.ts
+++ behavioural/mediator/baseComponent.ts
@@ -1,7 +1,7 @@
 import { Mediator } from './mediator';
 
 export class BaseComponent {
-    constructor(private mediator?: Mediator) { }
+    constructor(protected mediator?: Mediator) { }
 
     public setMediator(mediator: Mediator): void {
         this.mediator = mediator;
