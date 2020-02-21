28952d5f913803fba2885ab844632836e5b5a852
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 12:54:32 +0100

    Removed the setMediator method

--- behavioural/mediator/baseComponent.ts
+++ behavioural/mediator/baseComponent.ts
@@ -1,9 +1,5 @@
 import { Mediator } from './mediator';
 
 export class BaseComponent {
-    constructor(protected mediator?: Mediator) { }
-
-    public setMediator(mediator: Mediator): void {
-        this.mediator = mediator;
-    }
+    constructor(protected mediator: Mediator) { }
 }
