4d9fd45ff38c5fa9563a190a87d751595546113f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 13:07:42 +0100

    the component communicate through the client code

--- behavioural/mediator/client.ts
+++ behavioural/mediator/client.ts
@@ -1,4 +1,13 @@
 import { ConcreteMediator } from './concreteMediator';
 import { Component1, Component2 } from './components';
 
-// ..
+const c1 = new Component1();
+const c2 = new Component2();
+new ConcreteMediator(c1, c2);
+
+console.log('Client triggers operation A.');
+c1.doA();
+
+console.log('');
+console.log('Client triggers operation D.');
+c2.doD();
