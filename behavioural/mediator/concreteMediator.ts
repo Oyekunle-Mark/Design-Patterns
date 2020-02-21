a1f88ff0fdfe5a2fbfb6f4b28e4d2c3adfbb8982
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 12:59:46 +0100

    The concrete mediator notifies the right method of the action performed

--- behavioural/mediator/concreteMediator.ts
+++ behavioural/mediator/concreteMediator.ts
@@ -1,5 +1,19 @@
 import { Mediator } from './mediator';
+import { Component1, Component2 } from './components';
 
 export class ConcreteMediator implements Mediator {
-    // ..
+    constructor(private component1: Component1, private component2: Component2) { }
+
+    public notify(sender: object, event: string): void {
+        if (event === 'A') {
+            console.log('Mediator reacts on A and triggers following operations:');
+            this.component2.doC();
+        }
+
+        if (event === 'D') {
+            console.log('Mediator reacts on D and triggers following operations:');
+            this.component1.doB();
+            this.component2.doC();
+        }
+    }
 }
