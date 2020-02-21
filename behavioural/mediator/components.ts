88ed3da3de34178a576a2c124b2943f2a18a913b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 12:52:21 +0100

    The second component

--- behavioural/mediator/components.ts
+++ behavioural/mediator/components.ts
@@ -11,3 +11,15 @@ export class Component1 extends BaseComponent {
         this.mediator.notify(this, 'B');
     }
 }
+
+export class Component2 extends BaseComponent {
+    public doC(): void {
+        console.log('Component 2 does C.');
+        this.mediator.notify(this, 'C');
+    }
+
+    public doD(): void {
+        console.log('Component 2 does D.');
+        this.mediator.notify(this, 'D');
+    }
+}
