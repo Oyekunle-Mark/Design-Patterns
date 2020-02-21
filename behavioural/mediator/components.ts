d0cc021bb007ddada1147e1d63e0652c53ec6b57
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 12:52:02 +0100

    The first component

---
+++ behavioural/mediator/components.ts
@@ -0,0 +1,13 @@
+import { BaseComponent } from './baseComponent';
+
+export class Component1 extends BaseComponent {
+    public doA(): void {
+        console.log('Component 1 does A.');
+        this.mediator.notify(this, 'A');
+    }
+
+    public doB(): void {
+        console.log('Component 1 does B.');
+        this.mediator.notify(this, 'B');
+    }
+}
