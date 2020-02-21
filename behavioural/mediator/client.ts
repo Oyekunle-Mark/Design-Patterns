0db36bbec5e516b1f468d585db577c7a2c0f373a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-21 13:08:19 +0100

    Disable the no-new rule for the line

--- behavioural/mediator/client.ts
+++ behavioural/mediator/client.ts
@@ -3,6 +3,7 @@ import { Component1, Component2 } from './components';
 
 const c1 = new Component1();
 const c2 = new Component2();
+// eslint-disable-next-line no-new
 new ConcreteMediator(c1, c2);
 
 console.log('Client triggers operation A.');
