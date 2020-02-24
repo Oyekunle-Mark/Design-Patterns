235c05048416f5938098dca33705dd77eadaae4d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-24 10:14:26 +0100

    Generates a random string of length length

--- behavioural/originator.ts
+++ behavioural/originator.ts
@@ -4,4 +4,13 @@ export class Originator {
     constructor(private state: string) {
         console.log(`Originator: My initial state is: ${state}`);
     }
+
+    private generateRandomString(length = 10): string {
+        const charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        return Array(5)
+            .fill(' ')
+            .map(() => charSet.charAt(Math.floor(Math.random() * charSet.length)))
+            .join('');
+    }
 }
