793225a8b29d416a8c23f2c3957201d2a37b785c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-24 10:34:44 +0100

    The  caretaker manages commands

---
+++ behavioural/careTaker.ts
@@ -0,0 +1,32 @@
+import { Momento } from './momento';
+import { Originator } from './originator';
+
+export class CareTaker {
+    private momentos: Momento[] = [];
+
+    constructor(private originator: Originator) { }
+
+    public backup(): void {
+        console.log('\nCaretaker: Saving Originator\'s state...');
+        this.momentos.push(this.originator.save());
+    }
+
+    public undo(): void {
+        if (!this.momentos.length) {
+            return;
+        }
+        const momento = this.momentos.pop();
+
+        console.log(`Caretaker: Restoring state to: ${momento.getName()}`);
+        this.originator.restore(momento);
+    }
+
+    public showHistory(): void {
+        console.log('Caretaker: Here\'s the list of mementos:');
+
+        
+        for (const momento of this.momentos) {
+            console.log(momento.getName());
+        }
+    }
+}
