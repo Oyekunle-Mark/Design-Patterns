e697552dd090540a8c442bc0e9d3b09bdd445dcd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-24 09:47:37 +0100

    Concrete momento has state retrieval and meta data retrieval methods

--- behavioural/concreteMomento.ts
+++ behavioural/concreteMomento.ts
@@ -6,4 +6,16 @@ export class ConcreteMomento implements Momento {
     constructor(private state: string) {
         this.date = new Date().toISOString().slice(0, 19).replace('T', ' ');
     }
+
+    public getState(): string {
+        return this.state;
+    }
+
+    public getName(): string {
+        return `${this.date} / (${this.state.substr(0, 9)}...)`;
+    }
+
+    public getDate(): string {
+        return this.date;
+    }
 }
