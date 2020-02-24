42ecc7b7c8614cbef82f093e73a1176522a15408
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-24 10:36:44 +0100

    Renamed to memento

--- behavioural/careTaker.ts
+++ behavioural/careTaker.ts
@@ -2,31 +2,31 @@ import { Momento } from './momento';
 import { Originator } from './originator';
 
 export class CareTaker {
-    private momentos: Momento[] = [];
+    private memento: Momento[] = [];
 
     constructor(private originator: Originator) { }
 
     public backup(): void {
         console.log('\nCaretaker: Saving Originator\'s state...');
-        this.momentos.push(this.originator.save());
+        this.memento.push(this.originator.save());
     }
 
     public undo(): void {
-        if (!this.momentos.length) {
+        if (!this.memento.length) {
             return;
         }
-        const momento = this.momentos.pop();
+        const memento = this.memento.pop();
 
-        console.log(`Caretaker: Restoring state to: ${momento.getName()}`);
-        this.originator.restore(momento);
+        console.log(`Caretaker: Restoring state to: ${memento.getName()}`);
+        this.originator.restore(memento);
     }
 
     public showHistory(): void {
         console.log('Caretaker: Here\'s the list of mementos:');
 
         // eslint-disable-next-line no-restricted-syntax
-        for (const momento of this.momentos) {
-            console.log(momento.getName());
+        for (const memento of this.memento) {
+            console.log(memento.getName());
         }
     }
 }
