427190105d20cf175fcdfbf19d72aa74ff4a13e3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-24 16:27:43 +0100

    Added field state to the publisher interface

--- behavioural/observer/publisher.ts
+++ behavioural/observer/publisher.ts
@@ -1,6 +1,7 @@
 import { Subscriber } from './subscriber';
 
 export interface Publisher {
+    state: number;
     attach(subscriber: Subscriber): void;
     detach(subscriber: Subscriber): void;
     notify(): void;
