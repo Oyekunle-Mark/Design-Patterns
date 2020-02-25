c2f75dd8e21f3369b28e26683cb816fc36723a24
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-25 12:37:29 +0100

    UCreates the abstract class of teh state

---
+++ behavioural/state/state.ts
@@ -0,0 +1,11 @@
+export abstract class State {
+    private context: Context;
+
+    public setContext(context: Context): void {
+        this.context = context;
+    }
+
+    public abstract handle1(): void;
+
+    public abstract handle2(): void;
+}
