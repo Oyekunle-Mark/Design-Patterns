c1c5401a7834dfee1e12a59390ad7bb867c8c9b4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-25 12:49:22 +0100

    Every piece of state also extends the abstract base class

--- behavioural/state/concreteStates.ts
+++ behavioural/state/concreteStates.ts
@@ -11,3 +11,15 @@ export class ConcreteStateA extends State {
         console.log('ConcreteStateA handles request2.');
     }
 }
+
+export class ConcreteStateB extends State {
+    public handle1(): void {
+        console.log('ConcreteStateB handles request1.');
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateB handles request2.');
+        console.log('ConcreteStateB wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateA());
+    }
+}
