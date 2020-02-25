71c0ebedfbc05bb4be5458e8ab7e756a039596f7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-25 12:48:57 +0100

    The first state extends the base class

---
+++ behavioural/state/concreteStates.ts
@@ -0,0 +1,13 @@
+import { State } from './state';
+
+export class ConcreteStateA extends State {
+    public handle1(): void {
+        console.log('ConcreteStateA handles request1.');
+        console.log('ConcreteStateA wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateB());
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateA handles request2.');
+    }
+}
