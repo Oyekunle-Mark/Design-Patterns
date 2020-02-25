96d0c1135252d033c75753c6102e5a17de0c4b69
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-25 12:45:35 +0100

    The context sets the states

--- behavioural/state/context.ts
+++ behavioural/state/context.ts
@@ -1,5 +1,16 @@
 import { State } from './state';
 
 export class Context {
-    
+    private state: State;
+
+    constructor(state: State) {
+        this.transitionTo(state);
+    }
+
+    public transitionTo(state: State): void {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        console.log(`Context: Transition to ${(state as any).constructor.name}.`);
+        this.state = state;
+        this.state.setContext(this);
+    }
 }
