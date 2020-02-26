3725b787f5a19acfaec754ad0e14c2251859173a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 09:25:41 +0100

    Sets a strategy and does business logic

--- behavioural/strategy/context.ts
+++ behavioural/strategy/context.ts
@@ -2,4 +2,14 @@ import { Strategy } from './strategy';
 
 export class Context {
     constructor(private strategy: Strategy) { }
+
+    public setStrategy(strategy: Strategy): void {
+        this.strategy = strategy;
+    }
+
+    public doSomeBusinessLogic(): void {
+        console.log('Context: Sorting data using the strategy (not sure how it\'ll do it)');
+        const result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e']);
+        console.log(result.join(','));
+    }
 }
