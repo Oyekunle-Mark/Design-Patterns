3f99c18b7b314625b9c4a4465b9103e462ddd0b2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 09:51:44 +0100

    Concrete class extends the base class

---
+++ behavioural/template_method/concreteClasses.ts
@@ -0,0 +1,11 @@
+import { AbstractClass } from './abstractClass';
+
+export class ConcreteClass1 extends AbstractClass {
+    protected requiredOperations1(): void {
+        console.log('ConcreteClass1 says: Implemented Operation1');
+    }
+
+    protected requiredOperation2(): void {
+        console.log('ConcreteClass1 says: Implemented Operation2');
+    }
+}
