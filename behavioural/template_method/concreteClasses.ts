a3f50b1574a2d6deae40d62bc94b93231efe0b0b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 09:52:53 +0100

    Concrete class can extend any method it wants to

--- behavioural/template_method/concreteClasses.ts
+++ behavioural/template_method/concreteClasses.ts
@@ -9,3 +9,17 @@ export class ConcreteClass1 extends AbstractClass {
         console.log('ConcreteClass1 says: Implemented Operation2');
     }
 }
+
+export class ConcreteClass2 extends AbstractClass {
+    protected requiredOperations1(): void {
+        console.log('ConcreteClass2 says: Implemented Operation1');
+    }
+
+    protected requiredOperation2(): void {
+        console.log('ConcreteClass2 says: Implemented Operation2');
+    }
+
+    protected hook1(): void {
+        console.log('ConcreteClass2 says: Overridden Hook1');
+    }
+}
