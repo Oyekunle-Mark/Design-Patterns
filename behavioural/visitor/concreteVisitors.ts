38ce81d4da41c44c0d38a0f7ad371f3449106f8f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 12:41:04 +0100

    The first visitor has methods to perform operations on the concrete component

---
+++ behavioural/visitor/concreteVisitors.ts
@@ -0,0 +1,12 @@
+import { Visitor } from './visitor';
+import { ConcreteComponentA, ConcreteComponentB } from './concreteComponents';
+
+export class ConcreteVisitor1 implements Visitor {
+    public visitConcreteComponentA(element: ConcreteComponentA): void {
+        console.log(`${element.exclusiveMethodOfConcreteComponentA()} + ConcreteVisitor1`);
+    }
+
+    public visitConcreteComponentB(element: ConcreteComponentB): void {
+        console.log(`${element.specialMethodOfConcreteComponentB()} + ConcreteVisitor1`);
+    }
+}
