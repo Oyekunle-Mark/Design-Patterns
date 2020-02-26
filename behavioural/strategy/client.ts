af229a1ea12f4b6f4fcab1018e016e4d549a01cf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 09:29:54 +0100

    The client uses strategy to set the context

---
+++ behavioural/strategy/client.ts
@@ -0,0 +1,12 @@
+import { ConcreteStrategyA, ConcreteStrategyB } from './concreteStrategy';
+import { Context } from './context';
+
+const context = new Context(new ConcreteStrategyA());
+console.log('Client: Strategy is set to normal sorting.');
+context.doSomeBusinessLogic();
+
+console.log('');
+
+console.log('Client: Strategy is set to reverse sorting.');
+context.setStrategy(new ConcreteStrategyB());
+context.doSomeBusinessLogic();
