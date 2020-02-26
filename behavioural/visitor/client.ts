c31dcdd408e7f5d842683af24a24f5645e7b5e4b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-26 12:46:36 +0100

    Uses the concrete components and visitors

--- behavioural/visitor/client.ts
+++ behavioural/visitor/client.ts
@@ -9,3 +9,18 @@ function clientCode(components: Component[], visitor: Visitor): void {
         component.accept(visitor);
     }
 }
+
+const components = [
+    new ConcreteComponentA(),
+    new ConcreteComponentB(),
+];
+
+console.log('The client code works with all visitors via the base Visitor interface:');
+const visitor1 = new ConcreteVisitor1();
+clientCode(components, visitor1);
+
+console.log('');
+
+console.log('It allows the same client code to work with different types of visitors:');
+const visitor2 = new ConcreteVisitor2();
+clientCode(components, visitor2);
